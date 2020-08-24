3c831b35ea65819bc0f214f9672c7fa9461b1333
“zcybupt” <jp2016213431@qmul.ac.uk>
2020-08-24 16:26:40 +0800

    add 039

--- 039_combination_sum/Solution.go
+++ 039_combination_sum/Solution.go
@@ -14,13 +14,13 @@ func combinationSum(candidates []int, target int) [][]int {
 
     var path []int
     sort.Ints(candidates)
-    dfs(candidates, 0, arrLen, target, results, path)
+    dfs(candidates, 0, arrLen, target, &results, path)
     return results
 }
 
-func dfs(candidates []int, begin, arrLen, target int, results [][]int, path []int) {
+func dfs(candidates []int, begin, arrLen, target int, results *[][]int, path []int) {
     if target == 0 {
-        results = append(results, path)
+        *results = append(*results, append([]int{}, path...))
         return
     }
 
@@ -36,8 +36,8 @@ func dfs(candidates []int, begin, arrLen, target int, results [][]int, path []in
 }
 
 func main() {
-    nums := []int{2, 3, 6, 7}
-    target := 7
+    nums := []int{2, 3, 5}
+    target := 8
     results := combinationSum(nums, target)
     for _, result := range results {
         for _, ele := range result {
