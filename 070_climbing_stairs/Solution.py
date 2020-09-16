603326cc9cdc8cfe6d3024c2410dcd58478f87cf
“zcybupt” <jp2016213431@qmul.ac.uk>
2020-09-16 16:45:44 +0800

    add 070

--- 070_climbing_stairs/Solution.py
+++ 070_climbing_stairs/Solution.py
@@ -1,13 +1,12 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n < 4: return n
-        dp = [0] * n
+        dp = [0] * (n + 1)
         dp[0] = 1
-        dp[1] = 2
+        dp[1] = 1
 
-        for i in range(2, n): dp[i] = dp[i - 1] + dp[i - 2]
+        for i in range(2, n + 1): dp[i] = dp[i - 1] + dp[i - 2]
 
-        return dp[n - 1]
+        return dp[n]
 
 
 if __name__ == '__main__':
