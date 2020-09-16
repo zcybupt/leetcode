039f95aaef32fd2b168882bb5785312ebcbbe9d0
“zcybupt” <jp2016213431@qmul.ac.uk>
2020-09-16 16:32:25 +0800

    add 070

---
+++ 070_climbing_stairs/Solution.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func climbStairs(n int) int {
+    if n < 4 { return n }
+    dp := make([]int, n)
+    dp[0] = 1
+    dp[1] = 2
+
+    for i := 2; i < n; i++ {
+        dp[i] = dp[i - 1] + dp[i - 2]
+    }
+
+    return dp[n - 1]
+}
+
+func main() {
+    fmt.Println(climbStairs(10))
+}
