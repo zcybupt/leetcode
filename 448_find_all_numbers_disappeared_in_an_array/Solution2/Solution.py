5b82fddcffe510799c0f34f92c04afc2308be152
zcybupt <jp2016213431@qmul.ac.uk>
2020-09-30 09:52:52 +0800

    add 448

--- 448_find_all_numbers_disappeared_in_an_array/Solution2/Solution.py
+++ 448_find_all_numbers_disappeared_in_an_array/Solution2/Solution.py
@@ -1,13 +1,8 @@
 class Solution:
     def findDisappearedNumbers(self, nums: list) -> list:
         if not nums: return []
-
-        result = []
         for num in nums: nums[abs(num) - 1] = -abs(nums[abs(num) - 1])
-        for i, num in enumerate(nums):
-            if num > 0: result.append(i + 1)
-
-        return result
+        return [i + 1 for i, num in enumerate(nums) if num > 0]
 
 
 if __name__ == '__main__':
